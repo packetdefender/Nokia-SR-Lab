76242fe872a1b8dd0614852cafc7f015cee1b069
Mike Lossmann <michaellossmann@gmail.com>
2020-07-06 04:04:07 +0000

    Added IP Assignment Python Script

---
+++ ip_address_assign.py
@@ -0,0 +1,68 @@
+import uuid
+import sys
+from ruamel.yaml import YAML
+import os
+import re
+import ipaddress
+
+yaml = YAML()
+yaml.indent(mapping=4, sequence=4, offset=2)
+pwd = os.getcwd()
+hv_path = f'{pwd}/host_vars/'
+gv_path = f'{pwd}/group_vars/'
+
+print('*' * 35)
+print('''
+This script will generate BoF addresses and modify each router's 
+host_vars yml file for you based on the IP range that is input
+into the script.  The script assumes the default gateway is
+ends in .1
+''')
+print('*' * 35)
+input_rtr = int(input('How many routers are you deploying: '))
+
+
+def bof_address_assign(ir):
+    if ir >= 12:
+        ir = 12
+    num_rtr = list(range(1, ir + 1))
+    while True:
+        try:
+            ip = ipaddress.ip_network(
+                input("What is the IP address range you are looking for: "))
+            ip_range = list(ip.hosts())
+            available_ips = []
+            for i in ip_range:
+                ret = os.system(f'ping -o -c 2 -W 3000 {i}')
+                if ret != 0:
+                    a_ip = str(i)
+                    available_ips.append(a_ip)
+            if len(available_ips) < num_rtr[-1]:
+                print('You do not have enough IP addresses for your topology')
+                continue
+            default_gateway = available_ips[-1].split('.')
+            default_gateway[-1] = '1'
+            dg = ".".join(default_gateway)
+            for rtr in num_rtr:
+                with open(f'{hv_path}/R{rtr}.yml') as f:
+                    yml_doc = yaml.load(f)
+                    if 'vsr_mgmt' in yml_doc:
+                        yml_doc['vsr_mgmt'] = available_ips[rtr-1]
+                        yml_doc['vsr_mgmt_gw'] = dg
+                with open(f'{hv_path}/R{rtr}.yml', "w") as f:
+                    yaml.dump(yml_doc, f)
+
+            r_ipdict = {num_rtr[i]: available_ips[i]
+                        for i in range(len(num_rtr))}
+
+            for k, v in r_ipdict.items():
+                print(f"Rotuer{k} has a management IP address of: {v}")
+            print(f'Your default gateway is {dg}')
+            break
+        except ValueError:
+            print('Please only enter an IP address in dotted decimal format')
+        except TypeError:
+            print('Please only enter a number from 1 - 12')
+
+
+bof_address_assign(input_rtr)
